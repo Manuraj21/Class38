7a72a6ede16993737e8dbe2e0e81fb9a0c780118
Manuraj21 <kalavalamanuraj@gmail.com>
2020-11-19 16:52:09 +0530

    hi

--- js/Game.js
+++ js/Game.js
@@ -26,6 +26,15 @@ class Game {
       form = new Form()
       form.display();
     }
+    car1 = createSprite(100,200);
+    car2 = createSprite(300,200);
+    car3 = createSprite(500,200);
+    car4 = createSprite(700,200);
+    cars = [car1,car2,car3,car4];
+
+    
+
+
   }
 
   play(){
@@ -35,22 +44,34 @@ class Game {
     Player.getPlayerInfo();
 
     if(allPlayers !== undefined){
+      var index = 0;
+      var x = 0;
+      var y;
       var display_position = 130;
       for(var plr in allPlayers){
-        if (plr === "player" + player.index)
-          fill("red")
-        else
-          fill("black");
-
-        display_position+=20;
-        textSize(15);
-        text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position)
+        //changing the distance of x and y
+        index = index+1;
+        x = x+200;
+        y = displayHeight - allPlayers[plr].distance;
+        //the car's position is the player's position
+        cars[index-1].x = x;
+        cars[index-1].y = y;
+        if(index === player.index){
+          cars[index-1].shapecolor = "red";
+          camera.position.x = displayWidth/2;
+          camera.position.y = cars[index-1].y;
+        }
+        
+        
+       
       }
     }
 
     if(keyIsDown(UP_ARROW) && player.index !== null){
       player.distance +=50
       player.update();
+      //drawSprites();
     }
+    drawSprites();
   }
 }
